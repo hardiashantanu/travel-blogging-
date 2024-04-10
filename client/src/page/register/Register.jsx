6bcc54a3c91065956a8ba4597c820eb81e054db0
Tanish Parashar <163186359+parashartanish22@users.noreply.github.com>
2024-04-10 19:03:56 +0000

    register done

--- client/src/page/register/Register.jsx
+++ client/src/page/register/Register.jsx
@@ -1,45 +1,50 @@
-import { useState } from "react";
-import"./register.css"
 import { Link } from "react-router-dom";
 import axios from "axios";
-import React from "react";
+import { useState } from "react";
+
 export default function Register() {
-  const[username,setUsername]=useState("")
-  const[email,setEmail]=useState("")
-  const[password,setPassword]=useState("")
-  const[error,setError]=useState(false)
-  const handleSubmit= async (e)=>{
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     setError(false);
-    try{const res= await axios.post("/auth/register",{
-      username,
-      email,
-      password,
-    });
-    res.data&&window.location.replace("/login")
-  }
-    catch(err){
+    try {
+      const res = await axios.post("/auth/register", {
+        username,
+        email,
+        password,
+      });
+      res.data && window.location.replace("/login");
+      console.log(res)
+    } catch (err) {
       setError(true);
     }
-    
-    
   };
+
   return (
-    <div className="register">
-    <span className="registerTitle">Register</span>
-    <form className="registerForm" onSubmit={handleSubmit}>
-      <label>Username</label>
-      <input className="registerInput" type="text" placeholder="Enter your username..." onChange={e=>setUsername(e.target.value)} />
-      <label>Email</label>
-      <input className="registerInput" type="text" placeholder="Enter your email..." onChange={e=>setEmail(e.target.value)}/>
-      <label>Password</label>
-      <input className="registerInput" type="password" placeholder="Enter your password..."onChange={e=>setPassword(e.target.value)} />
-      <button className="registerButton" type="submit">Register</button>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <form className="p-8 bg-white rounded shadow-md" onSubmit={handleSubmit}>
+        <label className="input input-bordered flex items-center gap-3 mb-6">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-6 h-6 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
+          <input type="text" className="grow text-lg" placeholder="Username" onChange={e=>setUsername(e.target.value)} />
+        </label>
+        <label className="input input-bordered flex items-center gap-3 mb-6">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-6 h-6 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
+          <input type="email" className="grow text-lg" placeholder="Email" onChange={e=>setEmail(e.target.value)} />
+        </label>
+        <label className="input input-bordered flex items-center gap-3 mb-6">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-6 h-6 opacity-70"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
+          <input type="password" className="grow text-lg" placeholder="Password" onChange={e=>setPassword(e.target.value)} />
+        </label>
+        <button className="w-full p-3 text-xl text-white bg-blue-500 rounded hover:bg-blue-600" type="submit">Register</button>
       </form>
-      <button className="registerLoginButton">
-      <Link className="link" to="/login">LOGIN</Link>
+      <button className="btn p-3 text-xl mt-3">
+        <Link className="" to="/login">LOGIN</Link>
       </button>
-      {error&&<span style={{color:"red",marginTop:"10px"}}>Something went wrong</span>}
-  </div>
-  )
+      {error && <span className="mt-6 text-xl text-red-500">Something went wrong</span>}
+    </div>
+  );
 }
